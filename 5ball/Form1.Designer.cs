8c442d3f9c620ab63c441bfbd3e9930f540b8854
klimenta@iandreev.com <klimenta@iandreev.com>
2016-03-25 12:13:57 -0400

    First draft

---
+++ 5ball/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _5ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbExit = new System.Windows.Forms.PictureBox();
+            this.pbMinimize = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMinimize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbExit
+            // 
+            this.pbExit.Image = global::_5ball.Properties.Resources.close_off;
+            this.pbExit.Location = new System.Drawing.Point(444, 1);
+            this.pbExit.Name = "pbExit";
+            this.pbExit.Size = new System.Drawing.Size(32, 32);
+            this.pbExit.TabIndex = 1;
+            this.pbExit.TabStop = false;
+            this.pbExit.Click += new System.EventHandler(this.pbExit_Click);
+            this.pbExit.MouseEnter += new System.EventHandler(this.pbExit_MouseEnter);
+            this.pbExit.MouseLeave += new System.EventHandler(this.pbExit_MouseLeave);
+            // 
+            // pbMinimize
+            // 
+            this.pbMinimize.Image = global::_5ball.Properties.Resources.min_off;
+            this.pbMinimize.Location = new System.Drawing.Point(410, 1);
+            this.pbMinimize.Name = "pbMinimize";
+            this.pbMinimize.Size = new System.Drawing.Size(32, 32);
+            this.pbMinimize.TabIndex = 0;
+            this.pbMinimize.TabStop = false;
+            this.pbMinimize.Click += new System.EventHandler(this.pbMinimize_Click);
+            this.pbMinimize.MouseEnter += new System.EventHandler(this.pbMinimize_MouseEnter);
+            this.pbMinimize.MouseLeave += new System.EventHandler(this.pbMinimize_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(485, 407);
+            this.Controls.Add(this.pbExit);
+            this.Controls.Add(this.pbMinimize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbExit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMinimize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMinimize;
+        private System.Windows.Forms.PictureBox pbExit;
+    }
+}
+
---
+++ 5ball/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _5ball
+{
+    public partial class Form1 : Form
+    {
+
+        const int cGridElements = 9;
+        const int cLeftPadding = 32;
+        const int cTopPadding = 32;
+        const int cPictureSizeWidth = 50;
+        const int cPictureSizeHeight = 50;
+        int i, j;
+
+        public static PictureBox[,] arrPictureBox = new PictureBox[cGridElements, cGridElements];
+
+        public Form1()
+        {
+            InitializeComponent();            
+            for (i = 0; i < cGridElements; i++)                
+            {                                                
+                for (j = 0; j < cGridElements; j++)
+                {
+                    arrPictureBox[i, j] = new PictureBox();
+                    arrPictureBox[i, j].Parent = this;
+                    arrPictureBox[i, j].Top = cTopPadding + cPictureSizeHeight * j;
+                    arrPictureBox[i, j].Left = cLeftPadding + cPictureSizeWidth * i;
+                    arrPictureBox[i, j].Width = cPictureSizeWidth;
+                    arrPictureBox[i, j].Height = cPictureSizeHeight;
+                    //arrPictureBox[i, j].Image = ((System.Drawing.Image)(Properties.Resources._100Wx75H));
+                    arrPictureBox[i, j].MouseDown += pictureBox_MouseDown;
+                    arrPictureBox[i, j].AllowDrop = true;
+                    arrPictureBox[i, j].DragEnter += pictureBox_DragEnter;
+                    arrPictureBox[i, j].DragDrop += pictureBox_DragDrop;
+                    //arrPictureBox[i, j].GiveFeedback += pictureBox_GiveFeedback;
+                }
+            }
+            arrPictureBox[0, 0].Image = ((System.Drawing.Image)(Properties.Resources.green));
+            arrPictureBox[3, 5].Image = ((System.Drawing.Image)(Properties.Resources.green));
+            arrPictureBox[6, 8].Image = ((System.Drawing.Image)(Properties.Resources.green));
+
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var dragImage = (Bitmap)(sender as PictureBox).Image;
+                IntPtr icon = dragImage.GetHicon();
+                Cursor.Current = new Cursor(icon);
+                if (DoDragDrop((sender as PictureBox).Image, DragDropEffects.Copy) == DragDropEffects.Copy)
+                {
+                    (sender as PictureBox).Image = null;
+                }
+                DestroyIcon(icon);
+            }
+
+        }
+
+        private void pbMinimize_MouseLeave(object sender, EventArgs e)
+        {
+            pbMinimize.Image = ((System.Drawing.Image)(Properties.Resources.min_off));
+        }
+
+        private void pbMinimize_MouseEnter(object sender, EventArgs e)
+        {
+            pbMinimize.Image = ((System.Drawing.Image)(Properties.Resources.min_on));
+        }
+
+        private void pbExit_MouseEnter(object sender, EventArgs e)
+        {
+            pbExit.Image = ((System.Drawing.Image)(Properties.Resources.close_on));
+        }
+
+        private void pbExit_MouseLeave(object sender, EventArgs e)
+        {
+            pbExit.Image = ((System.Drawing.Image)(Properties.Resources.close_off));
+        }
+
+        private void pbMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void pbExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            e.UseDefaultCursors = false;
+        }
+        void pictureBox_DragEnter (object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Bitmap))) e.Effect = DragDropEffects.Copy;
+        }
+        void pictureBox_DragDrop(object sender, DragEventArgs e)
+        {
+            var bmp = (Bitmap)e.Data.GetData(typeof(Bitmap));
+            //var pb = new PictureBox();
+            //pb.Image = (Bitmap)e.Data.GetData(typeof(Bitmap));
+            //pb.Size = pb.Image.Size;
+            //pb.Location = this.PointToClient(new Point(e.X - pb.Width / 2, e.Y - pb.Height / 2));
+            //this.Controls.Add(pb);
+            (sender as PictureBox).Image = bmp;
+        }
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        extern static bool DestroyIcon(IntPtr handle);
+
+    }
+}
